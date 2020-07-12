4deb474728414c641867ad19284cb0b610ff1a9f
Bilal Tas <bilaltas@me.com>
2020-07-12 06:55:58 +0300

    Usage coming from API

--- src/components/molecules/Limitations.vue
+++ src/components/molecules/Limitations.vue
@@ -1,6 +1,8 @@
 <template>
 	<details class="plan-limitations">
-		<summary>Free Plan</summary>
+		<summary>
+			<span @click="fetchUsage">Free Plan</span>
+		</summary>
 		<div class="details-menu center">
 			<ul class="menu boxed">
 				<li class="title">
@@ -9,31 +11,31 @@
 				<li>
 					<span>
 						<span class="left">Projects</span>
-						<span class="right">{{ countProjects }}/{{ currentUser.max_projects }}</span>
+						<span class="right">{{ fetching ? "Loading" : usage.projects }}/{{ maxProjects }}</span>
 					</span>
 				</li>
 				<li>
 					<span>
-						<span class="left">Pages/Phases</span>
-						<span class="right">4/{{ currentUser.max_pages }}</span>
+						<span class="left">Pages and Phases</span>
+						<span class="right">{{ fetching ? "Loading" : usage.phases }}/{{ maxPages }}</span>
 					</span>
 				</li>
 				<li>
 					<span>
 						<span class="left">Screens</span>
-						<span class="right">4/{{ currentUser.max_screens }}</span>
+						<span class="right">{{ fetching ? "Loading" : usage.devices }}/{{ maxScreens }}</span>
 					</span>
 				</li>
 				<li>
 					<span>
 						<span class="left">Live Pins</span>
-						<span class="right">4/{{ currentUser.max_live_pins }}</span>
+						<span class="right">{{ fetching ? "Loading" : usage.livePins }}/{{ maxLivePins }}</span>
 					</span>
 				</li>
 				<li>
 					<span>
 						<span class="left">Comment Pins</span>
-						<span class="right">4/{{ currentUser.max_comment_pins }}</span>
+						<span class="right">{{ fetching ? "Loading" : usage.commentPins }}/{{ maxCommentPins }}</span>
 					</span>
 				</li>
 				<li>
@@ -49,19 +51,71 @@
 
 <script>
 	export default {
+		data() {
+			return {
+				fetching: false,
+				usage: {
+					projects: 0,
+					pages: 0,
+					devices: 0,
+					livePins: 0,
+					commentPins: 0
+				}
+			};
+		},
 		methods: {
-			fetchUsage() {}
+			async fetchUsage() {
+				this.$nextTick(() => {
+					this.$nuxt.$loading.start();
+				});
+
+				this.fetching = true;
+
+				await this.$axios
+					.get("usage")
+					.then(({ status, data }) => {
+						if (status === 200) {
+							const usage = data.usage;
+							console.log("USAGE: ", usage);
+
+							this.usage = usage;
+							this.fetching = false;
+						}
+					})
+					.catch(function(error) {
+						console.log("ERROR: ", error);
+						this.fetching = false;
+					});
+			}
 		},
 		computed: {
 			currentUser() {
 				return this.$auth.user;
 			},
-			countProjects() {
-				const myProjects = this.$store.getters["projects/get"].filter(
-					project => project.user_ID == this.currentUser.ID
-				);
-
-				return myProjects.length;
+			maxProjects() {
+				return this.currentUser.max_projects == "99999"
+					? "∞"
+					: this.currentUser.max_projects;
+			},
+			maxPages() {
+				return this.currentUser.max_pages == "99999"
+					? "∞"
+					: this.currentUser.max_pages;
+			},
+			maxScreens() {
+				return this.currentUser.max_screens == "99999"
+					? "∞"
+					: this.currentUser.max_screens;
+			},
+			maxLivePins() {
+				return this.currentUser.max_live_pins == "99999"
+					? "∞"
+					: this.currentUser.max_live_pins;
+			},
+			maxCommentPins() {
+				return this.currentUser.max_comment_pins == "99999"
+					? "∞"
+					: this.currentUser.max_comment_pins;
 			}
 		}
 	};
