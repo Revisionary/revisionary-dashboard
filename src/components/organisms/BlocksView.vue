db62f0bdeb3e0f67e15eaf4beea15ee2d75e9f41
Bilal Tas <bilaltas@me.com>
2020-07-19 21:12:16 +0300

    Ordering corrected

--- src/components/organisms/BlocksView.vue
+++ src/components/organisms/BlocksView.vue
@@ -120,9 +120,9 @@
 				return this.categories;
 			},
 			availableBlocks() {
-				return this.blocks.filter(
-					block => !block.archived && !block.deleted
-				);
+				return this.blocks
+					.filter(block => !block.archived && !block.deleted)
+					.sort(this.dynamicSortMultiple("order_number", "title"));
 			},
 			favoriteBlocks() {
 				return this.availableBlocks.filter(block => block.favorite);
@@ -165,9 +165,51 @@
 			blocksOfCategory(cat_ID) {
 				if (this.filter != null) return this.categorizedBlocks;
 
-				return this.categorizedBlocks.filter(function(block) {
-					return block.cat_ID == cat_ID;
-				});
+				return this.categorizedBlocks.filter(
+					block => block.cat_ID == cat_ID
+				);
+			},
+			dynamicSort(property) {
+				var sortOrder = 1;
+				if (property[0] === "-") {
+					sortOrder = -1;
+					property = property.substr(1);
+				}
+				return function(a, b) {
+					/* next line works with strings and numbers,
+					 * and you may want to customize it to your needs
+					 */
+					var result =
+						a[property] < b[property]
+							? -1
+							: a[property] > b[property]
+							? 1
+							: 0;
+					return result * sortOrder;
+				};
+			},
+			dynamicSortMultiple() {
+				let $this = this;
+
+				/*
+				 * save the arguments object as it will be overwritten
+				 * note that arguments object is an array-like object
+				 * consisting of the names of the properties to sort by
+				 */
+				var props = arguments;
+				return function(obj1, obj2) {
+					var i = 0,
+						result = 0,
+						numberOfProperties = props.length;
+					/* try getting a different result from 0 (equal)
+					 * as long as we have extra properties to compare
+					 */
+					while (result === 0 && i < numberOfProperties) {
+						result = $this.dynamicSort(props[i])(obj1, obj2);
+						i++;
+					}
+					return result;
+				};
 			}
 		}
 	};
