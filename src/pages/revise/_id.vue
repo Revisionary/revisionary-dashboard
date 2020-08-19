7b37f1ae45ffadc83f9f9f7799156b1d2825d36b
Bilal Tas <bilaltas@me.com>
2020-08-19 23:38:17 +0300

    Edited content is now showing up

--- src/pages/revise/_id.vue
+++ src/pages/revise/_id.vue
@@ -172,6 +172,14 @@
 				}
 				return elements[0];
 			},
+			iframeElementSelector(selector) {
+				let iframe = document.getElementById("the-page");
+				if (!iframe) return false;
+				let doc = iframe.contentDocument || iframe.contentWindow.document;
+				let elements = doc.querySelectorAll(selector);
+				if (!elements) return false;
+				return elements;
+			},
 			getElementOffset(element_index) {
 				var selectedElement = this.iframeElement(element_index);
 				if (!selectedElement) return false;
@@ -247,10 +255,98 @@
 				if (this.loaded) this.updatePinsLocations();
 				requestAnimationFrame(this.watchElementsPositions);
 			},
+			applyPinCSS() {
+				//console.log("Applying pins CSS...");
+				const stylePins = this.pins.filter((pin) => {
+					return pin.css !== null;
+				});
+
+				stylePins.forEach((pin) => {
+					var index = pin.element_index;
+					var element = this.iframeElement(index);
+					if (!element) {
+						console.log("Skipped because of no element");
+						return true;
+					}
+					console.log("Style change for element ", index);
+				});
+				//console.log("CSS APPLIED: ");
+			},
+			applyPinContent() {
+				//console.log("Applying pins contents...");
+				const contentPins = this.pins.filter((pin) => {
+					return pin.modification !== null;
+				});
+
+				contentPins.forEach((pin) => {
+					var index = pin.element_index;
+					var element = this.iframeElement(index);
+					if (!element) {
+						console.log("Skipped because of no element");
+						return true;
+					}
+					var tag = element.tagName.toUpperCase();
+
+					// If the type is HTML content change
+					if (pin.modification_type == "html") {
+						var newHTML = html_entity_decode(pin.modification);
+
+						// If edited element is a submit or reset input button
+						if (
+							tag == "INPUT" &&
+							(element.getAttribute("type") == "text" ||
+								element.getAttribute("type") == "email" ||
+								element.getAttribute("type") == "url" ||
+								element.getAttribute("type") == "tel" ||
+								element.getAttribute("type") == "submit" ||
+								element.getAttribute("type") == "reset")
+						) {
+							element.setAttribute("value", newHTML);
+							console.log(
+								"Value change for element #",
+								index,
+								tag,
+								newHTML
+							);
+						} else {
+							element.innerHTML = newHTML;
+							element.setAttribute("contenteditable", "true");
+							console.log(
+								"Content change for element #",
+								index,
+								tag,
+								newHTML
+							);
+						}
+					} else if (pin.modification_type == "image") {
+						var newSrc = pin.modification;
+
+						if (tag == "IMAGE")
+							element.setAttribute("xlink:href", newSrc);
+						else {
+							element.setAttribute("src", newSrc);
+							setTimeout(() => {
+								element.removeAttribute("srcset");
+							}, 1000);
+						}
+
+						console.log(
+							"Image Update for element #",
+							index,
+							tag,
+							newSrc
+						);
+					}
+				});
+				//console.log("CONTENTS APPLIED: ");
+			},
 			runInspector() {
 				console.log("INSPECTOR RUNNING");
 
 				this.watchElementsPositions();
+
+				this.applyPinCSS();
+				this.applyPinContent();
 			},
 		},
 		watch: {
@@ -272,8 +368,440 @@
 					});
 				}
 			},
+			pins() {
+				console.log("PINS CHANGEEEEEEEEEED");
+
+				this.applyPinCSS();
+				this.applyPinContent();
+			},
 		},
 	};
+
+	function get_html_translation_table(table, quote_style) {
+		//  discuss at: http://phpjs.org/functions/get_html_translation_table/
+		// original by: Philip Peterson
+		//  revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		// bugfixed by: noname
+		// bugfixed by: Alex
+		// bugfixed by: Marco
+		// bugfixed by: madipta
+		// bugfixed by: Brett Zamir (http://brett-zamir.me)
+		// bugfixed by: T.Wild
+		// improved by: KELAN
+		// improved by: Brett Zamir (http://brett-zamir.me)
+		//    input by: Frank Forte
+		//    input by: Ratheous
+		//        note: It has been decided that we're not going to add global
+		//        note: dependencies to php.js, meaning the constants are not
+		//        note: real constants, but strings instead. Integers are also supported if someone
+		//        note: chooses to create the constants themselves.
+		//   example 1: get_html_translation_table('HTML_SPECIALCHARS');
+		//   returns 1: {'"': '&quot;', '&': '&amp;', '<': '&lt;', '>': '&gt;'}
+
+		var entities = {},
+			hash_map = {},
+			decimal;
+		var constMappingTable = {},
+			constMappingQuoteStyle = {};
+		var useTable = {},
+			useQuoteStyle = {};
+
+		// Translate arguments
+		constMappingTable[0] = "HTML_SPECIALCHARS";
+		constMappingTable[1] = "HTML_ENTITIES";
+		constMappingQuoteStyle[0] = "ENT_NOQUOTES";
+		constMappingQuoteStyle[2] = "ENT_COMPAT";
+		constMappingQuoteStyle[3] = "ENT_QUOTES";
+
+		useTable = !isNaN(table)
+			? constMappingTable[table]
+			: table
+			? table.toUpperCase()
+			: "HTML_SPECIALCHARS";
+		useQuoteStyle = !isNaN(quote_style)
+			? constMappingQuoteStyle[quote_style]
+			: quote_style
+			? quote_style.toUpperCase()
+			: "ENT_COMPAT";
+
+		if (useTable !== "HTML_SPECIALCHARS" && useTable !== "HTML_ENTITIES") {
+			throw new Error("Table: " + useTable + " not supported");
+			// return false;
+		}
+
+		entities["38"] = "&amp;";
+		if (useTable === "HTML_ENTITIES") {
+			entities["38"] = "&amp;";
+			entities["60"] = "&lt;";
+			entities["62"] = "&gt;";
+			entities["160"] = "&nbsp;";
+			entities["161"] = "&iexcl;";
+			entities["162"] = "&cent;";
+			entities["163"] = "&pound;";
+			entities["164"] = "&curren;";
+			entities["165"] = "&yen;";
+			entities["166"] = "&brvbar;";
+			entities["167"] = "&sect;";
+			entities["168"] = "&uml;";
+			entities["169"] = "&copy;";
+			entities["170"] = "&ordf;";
+			entities["171"] = "&laquo;";
+			entities["172"] = "&not;";
+			entities["173"] = "&shy;";
+			entities["174"] = "&reg;";
+			entities["175"] = "&macr;";
+			entities["176"] = "&deg;";
+			entities["177"] = "&plusmn;";
+			entities["178"] = "&sup2;";
+			entities["179"] = "&sup3;";
+			entities["180"] = "&acute;";
+			entities["181"] = "&micro;";
+			entities["182"] = "&para;";
+			entities["183"] = "&middot;";
+			entities["184"] = "&cedil;";
+			entities["185"] = "&sup1;";
+			entities["186"] = "&ordm;";
+			entities["187"] = "&raquo;";
+			entities["188"] = "&frac14;";
+			entities["189"] = "&frac12;";
+			entities["190"] = "&frac34;";
+			entities["191"] = "&iquest;";
+			entities["192"] = "&Agrave;";
+			entities["193"] = "&Aacute;";
+			entities["194"] = "&Acirc;";
+			entities["195"] = "&Atilde;";
+			entities["196"] = "&Auml;";
+			entities["197"] = "&Aring;";
+			entities["198"] = "&AElig;";
+			entities["199"] = "&Ccedil;";
+			entities["200"] = "&Egrave;";
+			entities["201"] = "&Eacute;";
+			entities["202"] = "&Ecirc;";
+			entities["203"] = "&Euml;";
+			entities["204"] = "&Igrave;";
+			entities["205"] = "&Iacute;";
+			entities["206"] = "&Icirc;";
+			entities["207"] = "&Iuml;";
+			entities["208"] = "&ETH;";
+			entities["209"] = "&Ntilde;";
+			entities["210"] = "&Ograve;";
+			entities["211"] = "&Oacute;";
+			entities["212"] = "&Ocirc;";
+			entities["213"] = "&Otilde;";
+			entities["214"] = "&Ouml;";
+			entities["215"] = "&times;";
+			entities["216"] = "&Oslash;";
+			entities["217"] = "&Ugrave;";
+			entities["218"] = "&Uacute;";
+			entities["219"] = "&Ucirc;";
+			entities["220"] = "&Uuml;";
+			entities["221"] = "&Yacute;";
+			entities["222"] = "&THORN;";
+			entities["223"] = "&szlig;";
+			entities["224"] = "&agrave;";
+			entities["225"] = "&aacute;";
+			entities["226"] = "&acirc;";
+			entities["227"] = "&atilde;";
+			entities["228"] = "&auml;";
+			entities["229"] = "&aring;";
+			entities["230"] = "&aelig;";
+			entities["231"] = "&ccedil;";
+			entities["232"] = "&egrave;";
+			entities["233"] = "&eacute;";
+			entities["234"] = "&ecirc;";
+			entities["235"] = "&euml;";
+			entities["236"] = "&igrave;";
+			entities["237"] = "&iacute;";
+			entities["238"] = "&icirc;";
+			entities["239"] = "&iuml;";
+			entities["240"] = "&eth;";
+			entities["241"] = "&ntilde;";
+			entities["242"] = "&ograve;";
+			entities["243"] = "&oacute;";
+			entities["244"] = "&ocirc;";
+			entities["245"] = "&otilde;";
+			entities["246"] = "&ouml;";
+			entities["247"] = "&divide;";
+			entities["248"] = "&oslash;";
+			entities["249"] = "&ugrave;";
+			entities["250"] = "&uacute;";
+			entities["251"] = "&ucirc;";
+			entities["252"] = "&uuml;";
+			entities["253"] = "&yacute;";
+			entities["254"] = "&thorn;";
+			entities["255"] = "&yuml;";
+			entities["402"] = "&fnof;";
+			entities["913"] = "&Alpha;";
+			entities["914"] = "&Beta;";
+			entities["915"] = "&Gamma;";
+			entities["916"] = "&Delta;";
+			entities["917"] = "&Epsilon;";
+			entities["918"] = "&Zeta;";
+			entities["919"] = "&Eta;";
+			entities["920"] = "&Theta;";
+			entities["921"] = "&Iota;";
+			entities["922"] = "&Kappa;";
+			entities["923"] = "&Lambda;";
+			entities["924"] = "&Mu;";
+			entities["925"] = "&Nu;";
+			entities["926"] = "&Xi;";
+			entities["927"] = "&Omicron;";
+			entities["928"] = "&Pi;";
+			entities["929"] = "&Rho;";
+			entities["931"] = "&Sigma;";
+			entities["932"] = "&Tau;";
+			entities["933"] = "&Upsilon;";
+			entities["934"] = "&Phi;";
+			entities["935"] = "&Chi;";
+			entities["936"] = "&Psi;";
+			entities["937"] = "&Omega;";
+			entities["945"] = "&alpha;";
+			entities["946"] = "&beta;";
+			entities["947"] = "&gamma;";
+			entities["948"] = "&delta;";
+			entities["949"] = "&epsilon;";
+			entities["950"] = "&zeta;";
+			entities["951"] = "&eta;";
+			entities["952"] = "&theta;";
+			entities["953"] = "&iota;";
+			entities["954"] = "&kappa;";
+			entities["955"] = "&lambda;";
+			entities["956"] = "&mu;";
+			entities["957"] = "&nu;";
+			entities["958"] = "&xi;";
+			entities["959"] = "&omicron;";
+			entities["960"] = "&pi;";
+			entities["961"] = "&rho;";
+			entities["962"] = "&sigmaf;";
+			entities["963"] = "&sigma;";
+			entities["964"] = "&tau;";
+			entities["965"] = "&upsilon;";
+			entities["966"] = "&phi;";
+			entities["967"] = "&chi;";
+			entities["968"] = "&psi;";
+			entities["969"] = "&omega;";
+			entities["977"] = "&thetasym;";
+			entities["978"] = "&upsih;";
+			entities["982"] = "&piv;";
+			entities["8226"] = "&bull;";
+			entities["8230"] = "&hellip;";
+			entities["8242"] = "&prime;";
+			entities["8243"] = "&Prime;";
+			entities["8254"] = "&oline;";
+			entities["8260"] = "&frasl;";
+			entities["8472"] = "&weierp;";
+			entities["8465"] = "&image;";
+			entities["8476"] = "&real;";
+			entities["8482"] = "&trade;";
+			entities["8501"] = "&alefsym;";
+			entities["8592"] = "&larr;";
+			entities["8593"] = "&uarr;";
+			entities["8594"] = "&rarr;";
+			entities["8595"] = "&darr;";
+			entities["8596"] = "&harr;";
+			entities["8629"] = "&crarr;";
+			entities["8656"] = "&lArr;";
+			entities["8657"] = "&uArr;";
+			entities["8658"] = "&rArr;";
+			entities["8659"] = "&dArr;";
+			entities["8660"] = "&hArr;";
+			entities["8704"] = "&forall;";
+			entities["8706"] = "&part;";
+			entities["8707"] = "&exist;";
+			entities["8709"] = "&empty;";
+			entities["8711"] = "&nabla;";
+			entities["8712"] = "&isin;";
+			entities["8713"] = "&notin;";
+			entities["8715"] = "&ni;";
+			entities["8719"] = "&prod;";
+			entities["8721"] = "&sum;";
+			entities["8722"] = "&minus;";
+			entities["8727"] = "&lowast;";
+			entities["8730"] = "&radic;";
+			entities["8733"] = "&prop;";
+			entities["8734"] = "&infin;";
+			entities["8736"] = "&ang;";
+			entities["8743"] = "&and;";
+			entities["8744"] = "&or;";
+			entities["8745"] = "&cap;";
+			entities["8746"] = "&cup;";
+			entities["8747"] = "&int;";
+			entities["8756"] = "&there4;";
+			entities["8764"] = "&sim;";
+			entities["8773"] = "&cong;";
+			entities["8776"] = "&asymp;";
+			entities["8800"] = "&ne;";
+			entities["8801"] = "&equiv;";
+			entities["8804"] = "&le;";
+			entities["8805"] = "&ge;";
+			entities["8834"] = "&sub;";
+			entities["8835"] = "&sup;";
+			entities["8836"] = "&nsub;";
+			entities["8838"] = "&sube;";
+			entities["8839"] = "&supe;";
+			entities["8853"] = "&oplus;";
+			entities["8855"] = "&otimes;";
+			entities["8869"] = "&perp;";
+			entities["8901"] = "&sdot;";
+			entities["8968"] = "&lceil;";
+			entities["8969"] = "&rceil;";
+			entities["8970"] = "&lfloor;";
+			entities["8971"] = "&rfloor;";
+			entities["9001"] = "&lang;";
+			entities["9002"] = "&rang;";
+			entities["9674"] = "&loz;";
+			entities["9824"] = "&spades;";
+			entities["9827"] = "&clubs;";
+			entities["9829"] = "&hearts;";
+			entities["9830"] = "&diams;";
+			entities["338"] = "&OElig;";
+			entities["339"] = "&oelig;";
+			entities["352"] = "&Scaron;";
+			entities["353"] = "&scaron;";
+			entities["376"] = "&Yuml;";
+			entities["710"] = "&circ;";
+			entities["732"] = "&tilde;";
+			entities["8194"] = "&ensp;";
+			entities["8195"] = "&emsp;";
+			entities["8201"] = "&thinsp;";
+			entities["8204"] = "&zwnj;";
+			entities["8205"] = "&zwj;";
+			entities["8206"] = "&lrm;";
+			entities["8207"] = "&rlm;";
+			entities["8211"] = "&ndash;";
+			entities["8212"] = "&mdash;";
+			entities["8216"] = "&lsquo;";
+			entities["8217"] = "&rsquo;";
+			entities["8218"] = "&sbquo;";
+			entities["8220"] = "&ldquo;";
+			entities["8221"] = "&rdquo;";
+			entities["8222"] = "&bdquo;";
+			entities["8224"] = "&dagger;";
+			entities["8225"] = "&Dagger;";
+			entities["8240"] = "&permil;";
+			entities["8249"] = "&lsaquo;";
+			entities["8250"] = "&rsaquo;";
+			entities["8364"] = "&euro;";
+		}
+
+		if (useQuoteStyle !== "ENT_NOQUOTES") {
+			entities["34"] = "&quot;";
+		}
+		if (useQuoteStyle === "ENT_QUOTES") {
+			entities["39"] = "&#39;";
+		}
+
+		// ascii decimals to real symbols
+		for (decimal in entities) {
+			if (entities.hasOwnProperty(decimal)) {
+				hash_map[String.fromCharCode(decimal)] = entities[decimal];
+			}
+		}
+
+		return hash_map;
+	}
+
+	function htmlentities(string, quote_style, charset, double_encode) {
+		//  discuss at: http://phpjs.org/functions/htmlentities/
+		// original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		//  revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		//  revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		// improved by: nobbler
+		// improved by: Jack
+		// improved by: Rafał Kukawski (http://blog.kukawski.pl)
+		// improved by: Dj (http://phpjs.org/functions/htmlentities:425#comment_134018)
+		// bugfixed by: Onno Marsman
+		// bugfixed by: Brett Zamir (http://brett-zamir.me)
+		//    input by: Ratheous
+		//  depends on: get_html_translation_table
+		//        note: function is compatible with PHP 5.2 and older
+		//   example 1: htmlentities('Kevin & van Zonneveld');
+		//   returns 1: 'Kevin &amp; van Zonneveld'
+		//   example 2: htmlentities("foo'bar","ENT_QUOTES");
+		//   returns 2: 'foo&#039;bar'
+
+		var hash_map = this.get_html_translation_table(
+				"HTML_ENTITIES",
+				quote_style
+			),
+			symbol = "";
+
+		string = string == null ? "" : string + "";
+
+		if (!hash_map) {
+			return false;
+		}
+
+		if (quote_style && quote_style === "ENT_QUOTES") {
+			hash_map["'"] = "&#039;";
+		}
+
+		double_encode = double_encode == null || !!double_encode;
+
+		var regex = new RegExp(
+			"&(?:#\\d+|#x[\\da-f]+|[a-zA-Z][\\da-z]*);|[" +
+				Object.keys(hash_map)
+					.join("")
+					// replace regexp special chars
+					.replace(/([()[\]{}\-.*+?^$|\/\\])/g, "\\$1") +
+				"]",
+			"g"
+		);
+
+		return string.replace(regex, function (ent) {
+			if (ent.length > 1) {
+				return double_encode ? hash_map["&"] + ent.substr(1) : ent;
+			}
+
+			return hash_map[ent];
+		});
+	}
+
+	function html_entity_decode(string, quote_style) {
+		//  discuss at: http://phpjs.org/functions/html_entity_decode/
+		// original by: john (http://www.jd-tech.net)
+		//    input by: ger
+		//    input by: Ratheous
+		//    input by: Nick Kolosov (http://sammy.ru)
+		// improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		// improved by: marc andreu
+		//  revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		//  revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		// bugfixed by: Onno Marsman
+		// bugfixed by: Brett Zamir (http://brett-zamir.me)
+		// bugfixed by: Fox
+		//  depends on: get_html_translation_table
+		//   example 1: html_entity_decode('Kevin &amp; van Zonneveld');
+		//   returns 1: 'Kevin & van Zonneveld'
+		//   example 2: html_entity_decode('&amp;lt;');
+		//   returns 2: '&lt;'
+
+		var hash_map = {},
+			symbol = "",
+			tmp_str = "",
+			entity = "";
+		tmp_str = string.toString();
+
+		if (
+			false ===
+			(hash_map = get_html_translation_table("HTML_ENTITIES", quote_style))
+		) {
+			return false;
+		}
+
+		// fix &amp; problem
+		// http://phpjs.org/functions/get_html_translation_table:416#comment_97660
+		delete hash_map["&"];
+		hash_map["&"] = "&amp;";
+
+		for (symbol in hash_map) {
+			entity = hash_map[symbol];
+			tmp_str = tmp_str.split(entity).join(symbol);
+		}
+		tmp_str = tmp_str.split("&#039;").join("'");
+
+		return tmp_str;
+	}
 </script>
 
 <style lang="scss">
