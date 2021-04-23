d63c76e4f6226ded958e6a69ddd64627955b8b59
Bilal TAS <bilaltas@me.com>
2021-04-23 22:45:47 +0300

    New endpoint updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,7 +80,7 @@ export default {
 		// https: true,
 		// host: "dapi.revisionary.co",
 		// baseURL: "http://backend/v1/",
-		baseURL: process.env.API_ENDPOINT || 'https://new.revisionaryapp.com/v1/',
+		baseURL: process.env.API_ENDPOINT || 'https://npi.revisionary.co/v1/',
 		//baseURL: "/v1/",
 		// browserBaseURL: "https://new.revisionaryapp.com/v1/",
 		port: 80,
